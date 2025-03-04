2ee6205ab86009a98322393a7a481b65229d14ef
Andrew <andrewbelanda@gmail.com>
2025-03-04 16:45:03 +0900

    update

--- data.js
+++ data.js
@@ -8,20 +8,10 @@ var schools = [
        siswa: "622 orang",
        alamat: "Jln. Dr. Samratulangi Dok V Jayapura Utara",
        gambar: `<img src='img/sd hikmah yapis 1.jpeg' width='520vw' height='400vh'>`,
-       description: `<a href='img/sd hikmah yapis 1.jpeg' target='_blank'><img src='img/sd hikmah yapis 1.jpeg' width='150'></a><br>
-           Jln. Dr. Samratulangi Dok V Jayapura Utara.<br>
-           - Kepala Sekolah: MUCHADIYAH, S.Pd<br>
-           - Jumlah Guru: 32 orang<br>
-             - Guru Tetap: 12 orang<br>
-             - Guru Honor: 20 orang<br>
-           - Jumlah Murid: 622 orang<br>
-             - Kls I: 115 orang<br>
-             - Kls II: 107 orang<br>
-             - Kls III: 105 orang<br>
-             - Kls IV: 110 orang<br>
-             - Kls V: 93 orang<br>
-             - Kls VI: 92 orang<br>
-           - Jumlah Ruang Kelas: 18 ruang kelas`
+       description: `Jln. Dr. Samratulangi Dok V Jayapura Utara.<br>
+              Titik Lokasi : -2.529582203358798,140.71700108987818<br>
+           Jumlah Murid: 622 orang<br>
+           <a href='img/sd hikmah yapis 1.jpeg' target='_blank'><img src='img/sd hikmah yapis 1.jpeg' width='150'></a><br>`
    },
    { 
        name: "SD KARTIKA VI – JAYAPURA", 
@@ -31,44 +21,11 @@ var schools = [
        siswa: "895  orang",
        alamat: "Jln. Diponegoro No. 12 Jayapura",
        gambar: `<img src='img/tksdsmp kartika.jpeg' width='500vw' height='400vh'>`,
-       description: `<a href='img/tksdsmp kartika.jpeg' target='_blank'><img src='img/tksdsmp kartika.jpeg' width='150'></a><br>
-       <br>Jln. Diponegoro No. 12 Jayapura<br>
-No Tlp	 (0967) 534950 a<br>
-- Kepala Sekolah	: SUKARLAN, S.Pda<br>
-- Jumlah guru 		:  53 oranga<br>
-	- Guru tetap  	:  21 oranga<br>
-	- Guru honor	:  32 oranga<br>
-- Jumlah murid	: 895 oranga<br>
-	- Kls I		: 133 oranga<br>
-	- Kls II		: 158 oranga<br>
-	- Kls III	: 156 oranga<br>
-	- Kls IV		: 142 orang	<br>
-	- Kls V		: 157 oranga<br>
-	- Kls VI		: 149 oranga<br>
-- Jumlah ruang kelas 	:   24 ruang kelas.`
-   },
-   { 
-       name: "SMA Negeri 3 Jayapura", 
-       lat: -2.5300, 
-       lon: 140.7155, 
-       type: "SMA", 
-       siswa: "500 orang",
-       alamat: "Jln. Pemuda No. 5 Jayapura",
-       gambar: `<img src='img/sma_negeri_3.jpg' width='500vw' height='400vh'>`,
        description: `
-           Jln. Dr. Samratulangi Dok V Jayapura Utara.<br>
-           <a href='img/sd hikmah yapis 1.jpeg' target='_blank'><img src='img/sd hikmah yapis 1.jpeg' width='150'></a><br>
-           - Kepala Sekolah: MUCHADIYAH, S.Pd<br>
-           - Jumlah Guru: 32 orang<br>
-             - Guru Tetap: 12 orang<br>
-             - Guru Honor: 20 orang<br>
-           - Jumlah Murid: 622 orang<br>
-             - Kls I: 115 orang<br>
-             - Kls II: 107 orang<br>
-             - Kls III: 105 orang<br>
-             - Kls IV: 110 orang<br>
-             - Kls V: 93 orang<br>
-             - Kls VI: 92 orang<br>
-           - Jumlah Ruang Kelas: 18 ruang kelas`
-   }
+       <br>Jln. Diponegoro No. 12 Jayapura<br>
+       Titik Lokasi : -2.544070366266895, 140.69605458173442<br>
+       Jumlah murid	: 895 oranga<br>
+<a href='img/tksdsmp kartika.jpeg' target='_blank'><img src='img/tksdsmp kartika.jpeg' width='150'></a><br>`
+   },
+
 ];
